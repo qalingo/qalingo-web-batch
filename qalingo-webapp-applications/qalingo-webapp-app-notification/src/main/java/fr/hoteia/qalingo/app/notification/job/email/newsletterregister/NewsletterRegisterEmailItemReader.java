7cbebaa6c82d9a17f438ef31703e26f991636c92
denis <denis@Hoteia-Laptop.home>
2013-08-16 23:19:29 +0200

    add different email types

---
+++ qalingo-webapp-applications/qalingo-webapp-app-notification/src/main/java/fr/hoteia/qalingo/app/notification/job/email/newsletterregister/NewsletterRegisterEmailItemReader.java
@@ -0,0 +1,43 @@
+/**
+ * Most of the code in the Qalingo project is copyrighted Hoteia and licensed
+ * under the Apache License Version 2.0 (release version 0.7.0)
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *                   Copyright (c) Hoteia, 2012-2013
+ * http://www.hoteia.com - http://twitter.com/hoteia - contact@hoteia.com
+ *
+ */
+package fr.hoteia.qalingo.app.notification.job.email.newsletterregister;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemReader;
+
+import fr.hoteia.qalingo.app.notification.job.email.AbstractEmailItemReader;
+import fr.hoteia.qalingo.core.domain.Email;
+
+/**
+ * Thread-safe database {@link ItemReader} implementing the process indicator
+ * pattern.
+ * 
+ */
+public class NewsletterRegisterEmailItemReader<T> extends AbstractEmailItemReader<T> {
+
+	private final Logger LOG = LoggerFactory.getLogger(getClass());
+
+	@Override
+	protected List<Long> retrieveKeys() {
+		synchronized (lock) {
+			List<Long> keys = null;
+	    	try {
+    			keys = emailDao.findIdsForEmailSync(Email.EMAIl_TYPE_NEWSLETTER_REGISTRATION_CONFIRMATION);
+			} catch (Exception e) {
+				LOG.error("Error during the IDs loading", e);
+			} 
+			return keys;
+		}
+	}
+	
+}
