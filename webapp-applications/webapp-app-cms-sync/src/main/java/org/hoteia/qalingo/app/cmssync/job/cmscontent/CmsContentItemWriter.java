6ab1cfd398c909c855b60257c81abbbd624482ac
denis <denis@Hoteia-Laptop>
2013-11-14 22:37:53 +0100

    refactoring package name + folder name

---
+++ webapp-applications/webapp-app-cms-sync/src/main/java/org/hoteia/qalingo/app/cmssync/job/cmscontent/CmsContentItemWriter.java
@@ -0,0 +1,39 @@
+/**
+ * Most of the code in the Qalingo project is copyrighted Hoteia and licensed
+ * under the Apache License Version 2.0 (release version 0.7.0)
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *                   Copyright (c) Hoteia, 2012-2013
+ * http://www.hoteia.com - http://twitter.com/hoteia - contact@hoteia.com
+ *
+ */
+package org.hoteia.qalingo.app.cmssync.job.cmscontent;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.InitializingBean;
+
+import org.hoteia.qalingo.core.domain.CmsContent;
+
+
+/**
+ * 
+ */
+public class CmsContentItemWriter implements ItemWriter<CmsContent>, InitializingBean {
+	
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	public final void afterPropertiesSet() throws Exception {
+	}
+	
+	public void write(List<? extends CmsContent> processIndicatorItemWrapperList) throws Exception {
+		for (CmsContent targetCmsContent : processIndicatorItemWrapperList) {
+			
+	        
+		}
+	}
+
+}
---
+++ webapp-applications/webapp-app-erp-sync/src/main/java/org/hoteia/qalingo/app/erpsync/job/stock/StockItemProcessor.java
@@ -0,0 +1,43 @@
+/**
+ * Most of the code in the Qalingo project is copyrighted Hoteia and licensed
+ * under the Apache License Version 2.0 (release version 0.7.0)
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *                   Copyright (c) Hoteia, 2012-2013
+ * http://www.hoteia.com - http://twitter.com/hoteia - contact@hoteia.com
+ *
+ */
+package org.hoteia.qalingo.app.erpsync.job.stock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.InitializingBean;
+
+import org.hoteia.qalingo.core.batch.CommonProcessIndicatorItemWrapper;
+import org.hoteia.qalingo.core.domain.ProductSkuStock;
+
+
+/**
+ * 
+ */
+public class StockItemProcessor<T> implements ItemProcessor<CommonProcessIndicatorItemWrapper<ProductSkuStock, ProductSkuStock>, ProductSkuStock>, InitializingBean {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	public void afterPropertiesSet() throws Exception {
+	}
+
+	/**
+	 * Use the technical identifier to mark the input row as processed and
+	 * return unwrapped item.
+	 */
+	public ProductSkuStock process(CommonProcessIndicatorItemWrapper<ProductSkuStock, ProductSkuStock> wrapper) throws Exception {
+
+		ProductSkuStock sourceStock = wrapper.getItem();
+		
+			
+		return sourceStock;
+	}
+	
+}
